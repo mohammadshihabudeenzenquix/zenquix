d10935d70d65bed4fc429d30ba1ae35f7f9ae778
Suryaprkash03 <suryatheking5784@gmail.com>
2024-09-12 17:27:13 +0530

    hai

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import kws from '../images/clients/kws.png';
 import geps from '../images/clients/geps.png';
+import kws from '../images/clients/kws.png';
 import protergia from '../images/clients/protergia.png';
 
 const clientImage = {
@@ -13,25 +13,25 @@ const Clients = () => {
     return (
         <div className="mt-8 bg-gray-100">
             <section data-aos="fade-up">
-                <div className="my-4 py-4">
-                        <h2 className="my-2 text-center text-3xl text-blue-900 uppercase font-bold">Our Clients</h2>
+                <div className="py-4 my-4">
+                        <h2 className="my-2 text-3xl font-bold text-center text-blue-900 uppercase">Our Clients</h2>
                         <div className='flex justify-center'>
                             <div className='w-24 border-b-4 border-blue-900'></div>
                         </div>
-                        <h2 className="mt-4 mx-12 text-center text-xl lg:text-2xl font-semibold text-blue-900">Some of our clients.</h2>
+                        <h2 className="mx-12 mt-4 text-xl font-semibold text-center text-blue-900 lg:text-2xl">Some of our clients.</h2>
                     </div>
 
                 <div className="p-16" data-aos="fade-in" data-aos-delay="600">
                     <div className="grid sm:grid-cols-3 lg:grid-cols-3">
-                        <div style={clientImage} className= "overflow-hidden flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100 w-1/6">
+                        <div style={clientImage} className= "flex justify-center w-1/6 overflow-hidden transition-all ease-in-out opacity-50 hover:opacity-100">
                             <img src={kws} alt="client" />                           
                         </div>
 
-                        <div style={clientImage} className="overflow-hidden p-3 flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100">
-                            <img src={protergia} alt="client" />                            
+                        <div style={clientImage} className="flex justify-center p-3 overflow-hidden transition-all ease-in-out opacity-50 hover:opacity-100">
+                            <img src={protergia} alt="cliet" />                            
                         </div> 
 
-                        <div style={clientImage} className="overflow-hidden p-3 flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100">
+                        <div style={clientImage} className="flex justify-center p-3 overflow-hidden transition-all ease-in-out opacity-50 hover:opacity-100">
                             <img src={geps} alt="client" />                            
                         </div>
 
